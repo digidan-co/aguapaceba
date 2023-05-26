58151ee79be9a8ff7a848981fb9ff6c587511053
Digidan <digidan.panel@gmail.com>
2023-05-26 08:39:04 -0500

    Página Web versión de escritorio completa

---
+++ enviar.php
@@ -0,0 +1,17 @@
+<?php
+$nombre = $_POST['nombre'];
+$asunto = $_POST['asunto'];
+$correo = $_POST['correo'];
+$contenido = $_POST['contenido'];
+
+$destinatario = "aguapaceba@gmail.com";
+$asunto = "$asunto";
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "Asunto: $asunto \n";
+$carta .= "Contenido: $contenido \n";
+
+mail($destinatario, $correo, $asunto, $carta);
+header('Location:index.html')
+?>
